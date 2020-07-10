e9551461da6578d56c44a674f99b41ee2c5d0313
Disha Vaghamshi <dvvaghamshi18@gmail.com>
2020-07-10 14:38:24 -0400

    Initial Commit

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,49 @@
 class EventSourcer {
   constructor() {
+    // the value variable that will be modified on operations
     this.value = 0;
+
+    // the values array to store the passes in values to add/subtract
+    this.valuesStack = [];
+
+    //the undoredoStack to track the undo and redo operations
+    this.undoredoStack = [];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.valuesStack.push(num);
+    this.value = this.value + num;
+  }
+  subtract(num) {
+    this.valuesStack.push(-num);
+    this.value = this.value - num;
+  }
+  undo() {
+    if(this.valuesStack.length != 0) {
+      this.undoredoStack.push(this.valuesStack.pop());
+      this.value = this.value - this.undoredoStack[this.undoredoStack.length - 1];
+    }
+  }
+  redo() {
+    if(this.undoredoStack.length != 0) {
+      this.valuesStack.push(this.undoredoStack.pop());
+      this.value = this.value + this.valuesStack[this.valuesStack.length - 1];
+    }
+  }
+  bulk_undo(steps) {
+    while(this.valuesStack.length != 0 && steps > 0) {
+      steps--;
+      this.undoredoStack.push(this.valuesStack.pop());
+      this.value = this.value - this.undoredoStack[this.undoredoStack.length - 1]
+    }
+  }
+  bulk_redo(steps) {
+    while(this.undoredoStack.length != 0 && steps > 0) {
+      steps--;
+      this.valuesStack.push(this.undoredoStack.pop());
+      this.value = this.value + this.valuesStack[this.valuesStack.length - 1]
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
